e149e2989c07bb280c05f2a88a9238e618428ae1
i007102 <cai.chen@sap.com>
2017-04-02 19:10:16 +0800

    Java Basic : Data Type

---
+++ JavaBasic/src/test/java/basic/DataType.java
@@ -0,0 +1,44 @@
+package test.java.basic;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+		byte b = 123;
+		
+		System.out.print(b + "\n");
+		
+		short s = 1232;
+		
+		System.out.print(s + "\n");
+		
+		
+		int i = 5_2;
+		System.out.print(i + "\n");
+		
+		long l = 123213213l;
+		
+		System.out.print(l + "\n");
+		
+		float f = 123.4f;
+		
+		System.out.print(f + "\n");
+		
+		double d = 123.45678;
+		
+		System.out.print(d + "\n");
+		
+		
+		boolean resutl = true ;
+		
+		System.out.print(resutl + "\n");
+
+		char cChar = 'y';
+		System.out.print(cChar + "\n");
+		
+		String sString = "hello world";
+		
+		System.out.print(sString + "\n");
+	}
+
+}
