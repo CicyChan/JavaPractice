6151cf62738c2407bf35a73e2e213016c28a9a14
i007102 <cai.chen@sap.com>
2017-04-02 19:42:51 +0800

    Java Basic : Array Demo

---
+++ JavaBasic/src/test/java/basic/ArrayDemo.java
@@ -0,0 +1,39 @@
+package test.java.basic;
+import java.util.Arrays;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		
+		//Array initialization 
+		int[] anArray;
+        anArray = new int[3];
+           
+        anArray[0] = 100;
+        anArray[1] = 200;
+        anArray[2] = 300;
+
+        System.out.println("Element at index 0: " + anArray[0]);
+        
+        
+        //test array Copy
+        char[] copyFrom = { 'd', 'e', 'c', 'h', 'e', 'l', 'l',
+			    'o', 'n', 'a', 't', 'e', 'd' };
+        
+        char[] copyTo = new char[7];
+        
+        System.out.println(copyFrom);
+        
+        System.arraycopy(copyFrom, 3, copyTo, 0, 5);
+        
+        System.out.println(copyTo);
+        
+        //test array Manipulation
+        
+        char[] testCopyTo = java.util.Arrays.copyOfRange(copyFrom, 3, 8);
+        
+        System.out.println(testCopyTo);
+        
+    }
+
+}
