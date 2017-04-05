330d00ae8f7471bf12157014479b748d872528be
i007102 <cai.chen@sap.com>
2017-04-05 21:30:10 +0800

    Java Basic : Enum Test

---
+++ JavaBasic/src/test/java/oo/Day.java
@@ -0,0 +1,5 @@
+package test.java.oo;
+
+public enum Day {
+	SUNDAY, MONDAY, TUESDAY,WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
---
+++ JavaBasic/src/test/java/oo/EnumTest.java
@@ -0,0 +1,52 @@
+package test.java.oo;
+
+public class EnumTest {
+	
+	public Day day;
+
+	public EnumTest(Day day) {
+		this.day = day;
+	}
+	
+	public void tellTtLikeItIs(){
+		switch(day){
+		case MONDAY:
+			System.out.println("Monday are bad.");
+			break;
+		case TUESDAY:
+			System.out.println("TUESDAYs are bad.");
+			break;
+		case WEDNESDAY:
+			System.out.println("WEDNESDAYs are bad.");
+			break;
+		case THURSDAY:
+			System.out.println("THURSDAYs are bad.");
+			break;
+		case FRIDAY:
+			System.out.println("Fridays are better.");
+			break;
+		case SATURDAY:
+		case SUNDAY:
+			System.out.println("Weekend are best.");
+			break;
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		EnumTest firstDay = new EnumTest(Day.MONDAY);
+		
+		firstDay.tellTtLikeItIs();
+		
+		EnumTest Thriday = new EnumTest(Day.WEDNESDAY);
+		
+		Thriday.tellTtLikeItIs();
+		
+		EnumTest seventhDay = new EnumTest(Day.SUNDAY);
+		
+		seventhDay.tellTtLikeItIs();
+		
+
+	}
+
+}
