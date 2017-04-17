5cb1c0d800d96b957463b2f5d67a104bbee26dda
i007102 <cai.chen@sap.com>
2017-04-17 09:51:05 +0800

    javabasic: inhertiance

---
+++ JavaBasic/src/test/java/inheritance/RectanglePlus.java
@@ -0,0 +1,86 @@
+package test.java.inheritance;
+
+import test.java.oo.Point;
+
+public class RectanglePlus implements Relatable {
+	public int width = 0;
+	public int height = 0;
+	public Point origin;
+	
+	
+
+
+	public RectanglePlus() {
+		super();
+		
+		this.origin = new Point(0,0);
+	}
+
+
+	public RectanglePlus(Point origin) {
+		super();
+		this.origin = origin;
+	}
+
+
+	public RectanglePlus(int width, int height) {
+		super();
+		this.width = width;
+		this.height = height;
+		
+		this.origin = new Point(width, height);
+	}
+	
+	public void move(int x, int y){
+		this.origin.setX(x);
+		this.origin.setY(y);
+	}
+	
+	public int getArea(){
+		return this.getWidth() * this.getHeight();
+	}
+	
+	@Override
+	public int isLargerThan(Relatable other) {
+		RectanglePlus otherRect = (RectanglePlus) other;
+		
+		if(this.getArea() < otherRect.getArea()){
+			return -1;
+		}else if(this.getArea() > otherRect.getArea()){
+			return 1;
+		}else{
+			return 0;
+		}
+	}
+
+
+	public int getWidth() {
+		return width;
+	}
+
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+
+	public int getHeight() {
+		return height;
+	}
+
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+
+	public Point getOrigin() {
+		return origin;
+	}
+
+
+	public void setOrigin(Point origin) {
+		this.origin = origin;
+	}
+	
+}
---
+++ JavaBasic/src/test/java/inheritance/Relatable.java
@@ -0,0 +1,5 @@
+package test.java.inheritance;
+
+public interface Relatable {
+	public int isLargerThan(Relatable other);
+}
